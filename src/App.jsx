208ad45b6d49f85bb19f7aec2f97b41e98260621
Giulia-Ciampelli <g.ciampelli2505@gmail.com>
2024-11-15 14:51:34 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// importazioni
+import { useState } from 'react'
+import './App.css'
+import languages from './assets/languages';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
