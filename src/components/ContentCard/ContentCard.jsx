5a346a3b1618f3fba7150fc617e31535d06044c9
Giulia-Ciampelli <g.ciampelli2505@gmail.com>
2024-11-15 17:45:31 +0100

    docs: add and import card style module

--- src/components/ContentCard/ContentCard.jsx
+++ src/components/ContentCard/ContentCard.jsx
@@ -1,6 +1,9 @@
+// importazioni
+import style from '../ContentCard/ContentCard.module.css';
+
 export default function ContentCard({ language }) {
     return (
-        <div className='description'>
+        <div className={style.description}>
             <h2>
                 {language.title}
             </h2>
