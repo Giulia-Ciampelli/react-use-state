a33a0be333cd8a15eeaf7e705bce84522e2b2d4d
Giulia-Ciampelli <g.ciampelli2505@gmail.com>
2024-11-15 15:29:24 +0100

    docs: start onClick function

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,20 @@ function App() {
   // logica
   const [active, setActive] = useState(0);
 
+  // funzione per le tabs
+  function contentShow(e) {
+    const newActive = e.target.getAttribute('index');
+
+    setActive(newActive);
+  }
+
   // markup
   return (
     <>
       <div className="tabs">
         {languages.map((language, index) => (
-          <div className="tabs-item">
-            <h2>
+          <div className="tabs-item" key={index}>
+            <h2 onClick={contentShow} data-index={index}>
               {language.title}
             </h2>
             <div>
